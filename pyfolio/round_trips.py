1d16d9053c2a263021847f185c55632f67a74b77
vitaly <vitaly.antoshkin@gmail.com>
2021-11-06 21:01:07 +0300

    Fixes quantopian#656

--- pyfolio/round_trips.py
+++ pyfolio/round_trips.py
@@ -22,55 +22,55 @@ import numpy as np
 
 from .utils import print_table, format_asset
 
-PNL_STATS = OrderedDict(
-    [('Total profit', lambda x: x.sum()),
-     ('Gross profit', lambda x: x[x > 0].sum()),
-     ('Gross loss', lambda x: x[x < 0].sum()),
-     ('Profit factor', lambda x: x[x > 0].sum() / x[x < 0].abs().sum()
-      if x[x < 0].abs().sum() != 0 else np.nan),
-     ('Avg. trade net profit', 'mean'),
-     ('Avg. winning trade', lambda x: x[x > 0].mean()),
-     ('Avg. losing trade', lambda x: x[x < 0].mean()),
-     ('Ratio Avg. Win:Avg. Loss', lambda x: x[x > 0].mean() /
-      x[x < 0].abs().mean() if x[x < 0].abs().mean() != 0 else np.nan),
-     ('Largest winning trade', 'max'),
-     ('Largest losing trade', 'min'),
-     ])
+PNL_STATS = [
+    ('Total profit', lambda x: x.sum()),
+    ('Gross profit', lambda x: x[x > 0].sum()),
+    ('Gross loss', lambda x: x[x < 0].sum()),
+    ('Profit factor', lambda x: x[x > 0].sum() / x[x < 0].abs().sum()
+    if x[x < 0].abs().sum() != 0 else np.nan),
+    ('Avg. trade net profit', 'mean'),
+    ('Avg. winning trade', lambda x: x[x > 0].mean()),
+    ('Avg. losing trade', lambda x: x[x < 0].mean()),
+    ('Ratio Avg. Win:Avg. Loss', lambda x: x[x > 0].mean() /
+    x[x < 0].abs().mean() if x[x < 0].abs().mean() != 0 else np.nan),
+    ('Largest winning trade', 'max'),
+    ('Largest losing trade', 'min'),
+]
 
-SUMMARY_STATS = OrderedDict(
-    [('Total number of round_trips', 'count'),
-     ('Percent profitable', lambda x: len(x[x > 0]) / float(len(x))),
-     ('Winning round_trips', lambda x: len(x[x > 0])),
-     ('Losing round_trips', lambda x: len(x[x < 0])),
-     ('Even round_trips', lambda x: len(x[x == 0])),
-     ])
+SUMMARY_STATS = [
+    ('Total number of round_trips', 'count'),
+    ('Percent profitable', lambda x: len(x[x > 0]) / float(len(x))),
+    ('Winning round_trips', lambda x: len(x[x > 0])),
+    ('Losing round_trips', lambda x: len(x[x < 0])),
+    ('Even round_trips', lambda x: len(x[x == 0])),
+]
 
-RETURN_STATS = OrderedDict(
-    [('Avg returns all round_trips', lambda x: x.mean()),
-     ('Avg returns winning', lambda x: x[x > 0].mean()),
-     ('Avg returns losing', lambda x: x[x < 0].mean()),
-     ('Median returns all round_trips', lambda x: x.median()),
-     ('Median returns winning', lambda x: x[x > 0].median()),
-     ('Median returns losing', lambda x: x[x < 0].median()),
-     ('Largest winning trade', 'max'),
-     ('Largest losing trade', 'min'),
-     ])
+RETURN_STATS = [
+    ('Avg returns all round_trips', lambda x: x.mean()),
+    ('Avg returns winning', lambda x: x[x > 0].mean()),
+    ('Avg returns losing', lambda x: x[x < 0].mean()),
+    ('Median returns all round_trips', lambda x: x.median()),
+    ('Median returns winning', lambda x: x[x > 0].median()),
+    ('Median returns losing', lambda x: x[x < 0].median()),
+    ('Largest winning trade', 'max'),
+    ('Largest losing trade', 'min'),
+]
 
-DURATION_STATS = OrderedDict(
-    [('Avg duration', lambda x: x.mean()),
-     ('Median duration', lambda x: x.median()),
-     ('Longest duration', lambda x: x.max()),
-     ('Shortest duration', lambda x: x.min())
-     #  FIXME: Instead of x.max() - x.min() this should be
-     #  rts.close_dt.max() - rts.open_dt.min() which is not
-     #  available here. As it would require a new approach here
-     #  that passes in multiple fields we disable these measures
-     #  for now.
-     #  ('Avg # round_trips per day', lambda x: float(len(x)) /
-     #   (x.max() - x.min()).days),
-     #  ('Avg # round_trips per month', lambda x: float(len(x)) /
-     #   (((x.max() - x.min()).days) / APPROX_BDAYS_PER_MONTH)),
-     ])
+DURATION_STATS = [
+    ('Avg duration', lambda x: x.mean()),
+    ('Median duration', lambda x: x.median()),
+    ('Longest duration', lambda x: x.max()),
+    ('Shortest duration', lambda x: x.min())
+    #  FIXME: Instead of x.max() - x.min() this should be
+    #  rts.close_dt.max() - rts.open_dt.min() which is not
+    #  available here. As it would require a new approach here
+    #  that passes in multiple fields we disable these measures
+    #  for now.
+    #  ('Avg # round_trips per day', lambda x: float(len(x)) /
+    #   (x.max() - x.min()).days),
+    #  ('Avg # round_trips per month', lambda x: float(len(x)) /
+    #   (((x.max() - x.min()).days) / APPROX_BDAYS_PER_MONTH)),
+]
 
 
 def agg_all_long_short(round_trips, col, stats_dict):
